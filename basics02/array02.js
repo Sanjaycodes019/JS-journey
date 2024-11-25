8058a47dbc5c7a4d1982db9ab70348b552ae5d78
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2024-11-25 11:21:10 +0000

    commit

--- basics02/array02.js
+++ basics02/array02.js
@@ -1,1 +1,30 @@
-// 
+// //depth
+
+// const marvel_heroes = ["thor","ironman", "spiderman"];
+// const dc_heros = ["superman", "flash", "batman"];
+
+// // marvel_heroes.push(dc_heros);
+
+// // console.log(marvel_heroes); //[ 'thor', 'ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+
+// const allheroes = marvel_heroes.concat(dc_heros);
+// // console.log(allheroes); //[ 'thor', 'ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+// //to spread the array
+
+// const spreaded = [...marvel_heroes, ...dc_heros];
+// console.log(spreaded);
+
+
+const newarr = [ 2, 3, [3, 4, 5,[1, 1, 1], 8 ], [4, 5, 7], 4, 9];
+const realarray = newarr.flat(Infinity);
+console.log(newarr);
+console.log(realarray);
+
+console.log(Array.from("sanjay")); //converts that string to array
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
---
+++ basics02/objects.js
@@ -0,0 +1,67 @@
+//objects are created through literals, and constructors
+
+//Object.create  constructor method
+
+//defining a symbol
+// const mysym = Symbol("key1"); 
+
+// //object literals
+// const jsUser = {
+//     name: "Sanjay Gupta",
+//     age: 20,
+//     location: "nepal",
+//     email: "guptagroups09@gmail.com",
+//     isLoggedIn: false,
+//     [mysym]: "mykey1",
+//     days: ['monday', 'saturday']
+// }
+
+// //accessing the objects
+// console.log(jsUser.email);
+// console.log(jsUser["email"]); //best method
+
+// console.log(jsUser);
+
+// Object.freeze(jsUser); //after this any element can't be modified 
+
+
+//______________________________________________________________________________________________
+
+// const tinderUser = new Object() //singelton object
+// const tinderUser = {}  //non singleton object
+
+// tinderUser.id = '123abc';
+// tinderUser.name = 'sanjay';
+// tinderUser.isloggedin = true;
+
+// // console.log(tinderUser);
+
+// const regularUser = {
+//     email: "guptagroup09@gmail.com",
+//     fullname : {
+//         firstname : 'sanjay',
+//         lastname : 'gupta'
+//     }
+// }
+
+// console.log(regularUser.email);
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.firstname);
+
+
+const ob1 = { 1:'a', 2:'b'};
+const ob2 = { 3:'c', 4:'d'};
+
+// const ob3 = { ob1, ob2}
+// console.log(ob3);
+
+// const obj3 = Object.assign({}, ob1 ,ob2);  // object.assign( target, source)
+// console.log(obj3);  //concatenated successfully
+
+const obj3 = { ...ob1, ...ob2 };
+console.log(obj3); //this is the most used method in case of objects
+
+console.log(Object.keys(tinderuser)); // returns the key into array
+console.log(Object.values(tinderuser)); // returns values, into array
+
+// we can later iterate or play with that array too, storing in another variable
