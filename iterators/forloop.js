0c8934561308062e3ad7c8993082a2b9a3c0fc12
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2024-11-27 05:30:53 +0000

    commit

---
+++ iterators/forloop.js
@@ -0,0 +1,35 @@
+// for loop
+
+
+// for (let i = 1; i <= 10; i++){
+// const element = i;
+// console.log(element);
+// }
+
+
+let myArray = [ 'flash', 'batman', 'superman']
+//console.log("the size of the array is ", myArray.length);
+for (let i = 0; i < myArray.length; i++ ){
+    const element = myArray[i];
+    //console.log(element);
+}
+
+// break and continue
+
+// for ( let i = 0; i <= 20; i++){
+//     console.log(`value of i is ${i}`);
+//     if (i == 5 ) {
+//         console.log("detected 5");
+//         break;
+//     }
+// }
+
+//continue
+
+// for ( let i = 0; i <= 20; i++){
+//     if (i == 5 ) {
+//         console.log("detected 5");
+//         continue;
+//     }
+//     console.log(`value of i is ${i}`);
+// }
