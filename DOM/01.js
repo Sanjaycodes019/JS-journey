cda2782c3ff9be1d6361e92dc3dad052ee83aa49
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2024-11-29 08:06:56 +0000

    commit

---
+++ DOM/01.js
@@ -0,0 +1,30 @@
+// DOM manipulation in js
+
+/*
+document.getElementById('title').getAttribute('class')
+'heading'
+document.getElementById('title').setAttribute('class', 'test')
+undefined
+document.getElementById('title')
+<h1 id=​"title" class=​"test">​DOM learning in JS.​</h1>​
+const title = document.getElementById('title')
+undefined
+title
+<h1 id=​"title" class=​"test">​DOM learning in JS.​</h1>​
+title.style.backgroundcol
+undefined
+title.style.backgroundColor = 'green'
+'green'
+title.style.padding = '15px'
+'15px'
+title.style.borderRadius = '50%'
+'50%'
+title
+<h1 id=​"title" class=​"test" style=​"background-color:​ green;​ padding:​ 15px;​ border-radius:​ 50%;​">​DOM learning in JS.​</h1>​
+title.textContent
+'DOM learning in JS.'
+title.innerHTML
+'DOM learning in JS.'
+title.innerText
+'DOM learning in JS.'
+*/
