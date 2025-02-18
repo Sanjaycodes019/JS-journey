41d0767a597041ac468a8d0dd8c29438991e2a2e
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-02-18 17:40:56 +0000

    commit

--- 13recap/01.js
+++ 13recap/01.js
@@ -1,1 +1,19 @@
-// Framework vs Library
+/*
+
+framework
+-> complete tool that gives rules and structure
+for making website or app
+-> controls how our code runs, we must follow the rules
+-> bigger in size, many features like state management, routing
+-> less flexible, have to work the way it wants
+-> angular, vue.js, next.js (React with next)
+
+
+library
+-> collection of readymade functions, makes coding easier
+-> can control when and how to use library
+-> smaller in size, focuses on a new specific task
+-> more flexible, can be use as need
+-> react.js, jquery, lodash
+
+*/
